adfc8ea91421563cc99ad7bbdbd2e44561da5fc9
nivkap <84071374+nivkap@users.noreply.github.com>
2023-05-01 19:37:50 +0300

    part 2 - only 10 in a page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import type { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from '../lib/prisma'
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.post.findMany({
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const pageNumber = parseInt((query.page as string) || "1");
+  const postsPerPage = 10;
+  const postsAmount = await prisma.post.count({
     where: {
       published: true,
+    },
+  });
+
+  const totalNumberOfPages = Math.ceil(postsAmount / postsPerPage);
+
+  const feed = await getCurrentPagePosts(pageNumber)
+  
+  return {
+    props: { feed, pageNumber, totalNumberOfPages },
+  };
+
+};
+
+type Props = {
+  feed: PostProps[];
+  pageNumber: number;
+  totalNumberOfPages: number;
+};
+
+
+
+const getCurrentPagePosts = async (page:number) => {
+  const postsPerPage = 10;
+  const content = await prisma.post.findMany({
+    orderBy: {
+      id: 'asc'
+    },
+    where: {
+      published: true
+    },
+    take: postsPerPage * (page - 1)
+  })
+  .then(posts => {
+    if(posts.length === 0) 
+      return -1
+    return posts[posts.length-1].id
+  })
+  .then(prevId => prisma.post.findMany({
+    orderBy: {
+      id: 'asc'
+    },
+    take: postsPerPage,
+    where: {
+      published: true,
+      id: {
+        gt: prevId
+      }
     },
     include: {
       author: {
@@ -16,28 +65,51 @@ export const getServerSideProps: GetServerSideProps = async () => {
         },
       },
     },
-  });
-  return {
-    props: { feed },
-  };
-};
+  }))
+  return content;
+}  
 
-type Props = {
-  feed: PostProps[];
-};
 
 const Blog: React.FC<Props> = (props) => {
+
+  const changePage = (newPage: number) => {
+    window.location.href = `/?page=${newPage}`;
+  };
+
+  // Prepering the numbers of the buttons
+  const pageNumbers = [];
+  for (let i = 1; i <= props.totalNumberOfPages; i++) {
+    pageNumbers.push(i);
+  }
+  
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
+          <h2>Page {props.pageNumber} / {props.totalNumberOfPages}</h2>
+          <main>
+            {props.feed.map((post) => (
+              <div key={post.id} className="post">
+                <Post post={post} />
+              </div>
+            ))}
+          </main>
+        <nav>
+          <ul className="pagination">
+            {pageNumbers.map((number) => (
+              <li key={number} className="page-item">  {/* we can use button instead of a and remove href - changing the reloading*/}
+                <a
+                  href="#"
+                  className={`page-link ${props.pageNumber === number ? "active" : ""}`}
+                  onClick={() => changePage(number)}
+                >
+                  {number}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
       <style jsx>{`
         .post {
@@ -52,6 +124,26 @@ const Blog: React.FC<Props> = (props) => {
         .post + .post {
           margin-top: 2rem;
         }
+
+        .pagination {
+          display: flex;
+          overflow-x: auto;
+          white-space: nowrap;
+          padding-bottom: 1rem;
+          -ms-overflow-style: none; /* IE and Edge */
+          scrollbar-width: none; /* Firefox */
+        }
+        
+        .pagination::-webkit-scrollbar {
+          display: none; /* Hide scrollbar for Chrome, Safari and Opera */
+        }
+        
+        .page-item {
+          margin: 0 0.5rem;
+          display: inline-block;
+          white-space: nowrap;
+        }
+        
       `}</style>
     </Layout>
   );
